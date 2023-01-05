455eaf983713c313b72e827a43213cc5f9413f49
Joel Juhela <joel.juhela@protonmail.com>
2023-01-05 21:44:29 +0200

    Add JWT generation

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,12 @@
 import { Router } from "./deps/oak.ts";
 import * as gatewayController from "./controllers/gatewayController.ts";
+import * as authController from "./controllers/authController.ts";
 
 const router = new Router();
 
 router
-  .post("/insert-data", gatewayController.insertData);
-
-// TODO: require auth for some sensors?
-router
+  .post("/insert-data", gatewayController.insertData)
+  .post("/auth/login", authController.login)
   .get("/get-sensor-macs", gatewayController.getSensorMacsController);
 
 export default router;
