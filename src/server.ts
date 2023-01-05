19360d64a25081dcdca899857e6fd7ee6b99cb1c
Joel Juhela <joel.juhela@protonmail.com>
2023-01-05 10:35:43 +0200

    Add routes to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import { Application } from "https://deno.land/x/oak@v9.0.0/mod.ts";
+import router from "./routes.ts";
 import initDB from "./models/initDB.ts";
 
 const app = new Application();
+app.use(router.routes());
 initDB()
 
 await app.listen({port: 8000})
