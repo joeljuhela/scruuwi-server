ba57c205655c450a8be732b1a2c0e1471402dd9f
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2023-01-09 22:29:53 +0200

    Use in-memory sqlite for tests

--- src/models/db.ts
+++ src/models/db.ts
@@ -1,6 +1,8 @@
 import { DB } from "https://deno.land/x/sqlite@v3.7.0/mod.ts";
 import config from "../config.ts";
 
-const db = new DB(`./db/${config.env}.sqlite3`);
+const env = config.env;
+
+const db = env === "test" ? new DB() : new DB(`./db/${env}.sqlite3`);
 
 export default db;
